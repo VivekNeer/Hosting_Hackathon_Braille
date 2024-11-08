6795f80c1a77667284d982586cdf6a99725eb500
Vivek Neeralagi <139262048+VivekNeer@users.noreply.github.com>
2024-11-08 18:24:12 +0530

    Initial Files Setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import CustomNavbar from "./components/CustomNavbar";
+
+function App() {
+
+  return (
+    <>
+    <div className="flex flex-col min-h-screen">
+    <CustomNavbar/>
+    <div className="flex-grow relative">
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+      </Routes>
+      </div>
+      <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CustomNavbar.jsx
@@ -0,0 +1,24 @@
+// import { Link } from "react-router-dom";
+// import { TfiAlignJustify } from "react-icons/tfi";
+// import Rubiks from "../assets/squares.json";
+import { themeChange } from "theme-change";
+
+import {  useEffect } from "react";
+
+const CustomNavbar = () => {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+
+  return (
+    <div className="navbar bg-base-100 mb-4">
+        <select data-choose-theme className="btn hidden lg:block">
+          <option value="luxury">Luxury</option>
+          <option value="cupcake">Cupcake</option>
+          <option value="cyberpunk">Cyberpunk</option>
+        </select>
+    </div>
+  );
+};
+
+export default CustomNavbar;
